1ff21f77beb3455cf628dccc703adb8f37fe1048
Kamran Janjua <kamranejaz98@gmail.com>
2020-05-07 10:30:29 +0500

    Added transition page

--- code/front_end.py
+++ code/front_end.py
@@ -6,35 +6,72 @@ from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 from tkinter import filedialog
+from PIL import ImageTk,Image  
 
-class TkinterRoot(Tk):
-    def __init__(self):
-        super(TkinterRoot, self).__init__()
+
+class Starter(Tk):
+    def __init__(self, ):
+        super(Starter, self).__init__()
         self.title("Beaver - The .bib Editor")
-        
-        self.frame = ttk.Frame(self)
-        self.frame.pack()
+        container = ttk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
 
-        self.label_text = ttk.Label(self.frame, text="Enter Paper Name")
+        for F in (InitialPage, TkinterRoot):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+       
+        self.show_frame(InitialPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class InitialPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        super(InitialPage, self).__init__()
+        tk.Frame.__init__(self, parent)
+
+        canvas = Canvas(self, width=300, height=300)      
+        canvas.pack()      
+        img = ImageTk.PhotoImage(Image.open("/Users/Janjua/Desktop/logo.png"))  
+        canvas.create_image(300,300, anchor=NW, image=img)
+
+        self.button_start = ttk.Button(self, text="Welcome to Beaver", command=lambda: controller.show_frame(TkinterRoot))
+        self.button_start.pack()
+    
+
+class TkinterRoot(tk.Frame):
+    def __init__(self, parent, controller):
+        super(TkinterRoot, self).__init__()
+        tk.Frame.__init__(self, parent)
+
+        self.label_text = ttk.Label(self, text="Enter Paper Name")
         self.label_text.pack()
-        self.textBox = Text(self.frame, height=2, width=100)
+        self.textBox = Text(self, height=2, width=100)
         self.textBox.pack()
 
-        self.label_bib_ret = ttk.Label(self.frame, text="Retrieved Bib For Paper")
+        self.label_bib_ret = ttk.Label(self, text="Retrieved Bib For Paper")
         self.label_bib_ret.pack()
-        self.bib_ret_box = Text(self.frame, height=20, width=100)
+        self.bib_ret_box = Text(self, height=20, width=100)
         self.bib_ret_box.pack()
 
-        self.label_text_bib = ttk.Label(self.frame, text="Read .bib File Contents")
+        self.label_text_bib = ttk.Label(self, text="Read .bib File Contents")
         self.label_text_bib.pack()
-        self.outbox = Text(self.frame, height=20, width=100)
+        self.outbox = Text(self, height=20, width=100)
         self.outbox.pack()
 
-        self.button = ttk.Button(self.frame, text="Browse", command=self.fileDialog)
+        self.button = ttk.Button(self, text="Browse", command=self.fileDialog)
         self.button.pack()
-        self.button_change = ttk.Button(self.frame, text="Save", command=self.saveFile())
+
+        self.button_change = ttk.Button(self, text="Save", command=self.saveFile())
         self.button_change.pack()
-        self.button_retrieve = ttk.Button(self.frame, text="Retrieve Bib", command=self.saveFile())
+        
+        self.button_retrieve = ttk.Button(self, text="Retrieve Bib", command=self.saveFile())
         self.button_retrieve.pack()
 
     def fileDialog(self):
@@ -44,10 +81,9 @@ class TkinterRoot(Tk):
         read_file = open(self.file_name_updated, 'rb')
         print(read_file)
         print(self.file_name_updated)
-    
+        
     def saveFile(self):
         pass
 
-tk = TkinterRoot()
-tk.geometry('800x800')
+tk = Starter()
 tk.mainloop()
